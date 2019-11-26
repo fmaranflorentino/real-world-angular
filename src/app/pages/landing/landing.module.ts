b0a05b2b27ead64d6f769f04af1cb6c8271bf0e8
Flávio <florentino.flavioweb@gmail.com>
2019-11-26 16:54:09 -0300

    feat(landing): Created landing module

---
+++ src/app/pages/landing/landing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingRoutingModule } from './landing-routing.module';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { LandingComponent } from './landing/landing.component';
+import { BannerCarouselComponent } from './components/banner-carousel/banner-carousel.component';
+import { ProductsCarouselComponent } from './components/products-carousel/products-carousel.component';
+import { StoresCarouselComponent } from './components/stores-carousel/stores-carousel.component';
+import { SimulatorComponent } from './components/simulator/simulator.component';
+import { StepByStepComponent } from './components/step-by-step/step-by-step.component';
+import { FacilitiesComponent } from './components/facilities/facilities.component';
+import { SellersComponent } from './components/sellers/sellers.component';
+
+
+@NgModule({
+  declarations: [LandingComponent, BannerCarouselComponent, ProductsCarouselComponent, StoresCarouselComponent, SimulatorComponent, StepByStepComponent, FacilitiesComponent, SellersComponent],
+  imports: [
+    CommonModule,
+    LandingRoutingModule,
+    SharedModule,
+  ]
+})
+export class LandingModule { }
