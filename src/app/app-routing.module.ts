c9dd9dded0bf77f25819870805280cd988ca51bd
Flávio <florentino.flavioweb@gmail.com>
2019-11-27 08:46:00 -0300

    feat(routing): Created essential routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,13 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule)
-  }
+  },
+  { path: 'produtos', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  { path: 'parceiros', loadChildren: () => import('./pages/partners/partners.module').then(m => m.PartnersModule) },
+  { path: 'beneficios', loadChildren: () => import('./pages/benefits/benefits.module').then(m => m.BenefitsModule) },
+  { path: 'simulador', loadChildren: () => import('./pages/simulator/simulator.module').then(m => m.SimulatorModule) },
+  { path: 'sobre', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'financiamento', loadChildren: () => import('./pages/financing/financing.module').then(m => m.FinancingModule) }
 ];
 
 @NgModule({
