cefe66f71b6cb4ba7a5cac9dfcc8ed2da80e0a75
Flávio <florentino.flavioweb@gmail.com>
2019-11-27 08:44:19 -0300

    feat(pages): Created financing module

---
+++ src/app/pages/financing/financing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FinancingRoutingModule } from './financing-routing.module';
+import { FinancingComponent } from './financing.component';
+
+
+@NgModule({
+  declarations: [FinancingComponent],
+  imports: [
+    CommonModule,
+    FinancingRoutingModule
+  ]
+})
+export class FinancingModule { }
