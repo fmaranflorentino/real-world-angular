1b426e0d3e7f12ab7b71cafd14e2cbeeb01f60b5
Flávio <florentino.flavioweb@gmail.com>
2019-11-29 19:39:43 -0300

    feat(app): Created animations for route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { slideInAnimation } from './shared/helpers/animations';
+import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ slideInAnimation ]
 })
 export class AppComponent {
   title = 'marketclub-ui';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
