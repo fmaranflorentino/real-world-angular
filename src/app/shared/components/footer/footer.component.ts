a8ca23a03797214dfb8a2388c2aa9f6c742f76ed
Flávio <florentino.flavioweb@gmail.com>
2019-11-29 19:36:54 -0300

    feat(shared/components): Changes for footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn } from '../../helpers/animations';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  animations: [ fadeIn ],
 })
 export class FooterComponent implements OnInit {
 
