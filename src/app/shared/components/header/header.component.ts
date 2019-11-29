86b78ce1f3eb7894d001643170a239c02d5916ad
Flávio <florentino.flavioweb@gmail.com>
2019-11-29 19:29:54 -0300

    feat(shared/components): Changes for header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { fadeIn } from '../../helpers/animations';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [ fadeIn ],
 })
 export class HeaderComponent implements OnInit {
+  faHeart = faHeart;
 
   constructor() { }
 
