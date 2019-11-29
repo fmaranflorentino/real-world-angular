3303f49676d63f8d33459e73ed3eb376c6ac601d
Flávio <florentino.flavioweb@gmail.com>
2019-11-29 19:36:02 -0300

    feat(landing/sellers): Changes for sellers component

--- src/app/pages/landing/components/sellers/sellers.component.ts
+++ src/app/pages/landing/components/sellers/sellers.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { fadeIn, scaleOut, scaleIn } from 'src/app/shared/helpers/animations';
+import { wait } from 'src/app/shared/helpers/ui.helper';
+
 @Component({
   selector: 'app-sellers',
   templateUrl: './sellers.component.html',
-  styleUrls: ['./sellers.component.scss']
+  styleUrls: ['./sellers.component.scss'],
+  animations: [ fadeIn, scaleOut, scaleIn ]
 })
 export class SellersComponent implements OnInit {
+  firstStep = true;
+  secondStep = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleStep(): void {
+    this.firstStep = !this.firstStep;
+
+    wait(498)
+      .then(() => this.secondStep = !this.firstStep);
+
   }
 
 }
