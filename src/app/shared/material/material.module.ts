fc0e5074d4c7427a95e43cb1cbe9c2f3cba33ae3
Flávio <florentino.flavioweb@gmail.com>
2019-11-29 19:30:32 -0300

    feat(shared/material): Added more components and modules

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule, MatMenuModule, MatInputModule, MatRippleModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule, MatInputModule, MatRippleModule, MatExpansionModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
@@ -10,13 +10,17 @@ import { MatButtonModule, MatMenuModule, MatInputModule, MatRippleModule } from 
     MatMenuModule,
     MatButtonModule,
     MatInputModule,
-    MatRippleModule
+    MatRippleModule,
+    MatExpansionModule,
+    MatIconModule
   ],
   exports: [
     MatMenuModule,
     MatButtonModule,
     MatInputModule,
-    MatRippleModule
+    MatRippleModule,
+    MatExpansionModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
