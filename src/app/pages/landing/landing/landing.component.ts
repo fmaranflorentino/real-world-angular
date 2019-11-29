7dea4efe041c8fc937611beae3fe3347772a507c
Flávio <florentino.flavioweb@gmail.com>
2019-11-29 19:32:58 -0300

    feat(landing): Changes for landing component

--- src/app/pages/landing/landing/landing.component.ts
+++ src/app/pages/landing/landing/landing.component.ts
@@ -14,6 +14,21 @@ export class LandingComponent implements OnInit {
   productsOpts = productsOptions;
   storesOpts = storesOptions;
 
+
+  steps = [
+    { title: 'Simule', text: ' Sem compromisso' },
+    { title: 'Simule2', text: ' Sem compromisso' },
+    { title: 'Simule', text: ' Sem compromisso' },
+    { title: 'Simule', text: ' Sem compromisso' },
+    { title: 'Simule', text: ' Sem compromisso' },
+    { title: 'Simule', text: ' Sem compromisso' },
+    { title: 'Simule', text: ' Sem compromisso' },
+    { title: 'Simule', text: ' Sem compromisso' },
+  ];
+
+
+  stepByStepSelected = this.steps[0];
+
   value = 100;
   options: Options = {
     floor: 1000,
@@ -25,9 +40,30 @@ export class LandingComponent implements OnInit {
     ceil: 48
   };
 
+  indicators = [
+    { icon: 'assets/img/icon1.png', title: '1.000.000', desc: 'Clientes já cadastrados' },
+    { icon: 'assets/img/icon1.png', title: '15.000', desc: 'Pontos de venda' },
+    { icon: 'assets/img/icon1.png', title: 'R$ 10.000.000', desc: 'Economizados anualmente por nossos clientes' }
+  ];
+
+  leadBannerItems = [
+    {
+      title: 'Financiamento rápido para você comprar agora',
+      desc: ' Compre pelo nosso site o Market Club financia suas compras com menor taxa de juros do mercado.'
+    },
+    {
+      title: 'Beneficios rápidos para você comprar agora',
+      desc: ' Compre pelo nosso site o Market Club financia suas compras com menor taxa de juros do mercado.'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSelectedStep(step: {title: string, text: string}): void {
+    this.stepByStepSelected = {...step};
   }
 
 }
