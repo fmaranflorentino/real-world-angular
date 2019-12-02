5b6da9934bf0c1d6860a5aaea5e4dcff99f7e768
Flávio <florentino.flavioweb@gmail.com>
2019-12-02 09:59:35 -0300

    feat(dashboard/components): Created components for my-information and my-products

---
+++ src/app/pages/dashboard/components/my-information/my-information.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyInformationComponent } from './my-information.component';
+
+describe('MyInformationComponent', () => {
+  let component: MyInformationComponent;
+  let fixture: ComponentFixture<MyInformationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MyInformationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyInformationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MyProductsComponent } from './components/my-products/my-products.component';
+import { MyInformationComponent } from './components/my-information/my-information.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, MyProductsComponent, MyInformationComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
