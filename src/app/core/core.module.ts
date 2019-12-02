86dde4e53b40357ff9dad5960bdc2a465e474c8f
Flávio <florentino.flavioweb@gmail.com>
2019-12-02 15:01:15 -0300

    feat(core): Added errors handlers to the module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,18 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule, Optional, SkipSelf, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApplicationErrorHandler } from './handlers/application-error.interceptor';
+import { ServerErrorInterceptor } from './handlers/server-error.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [
+    { provide: ErrorHandler, useClass: ApplicationErrorHandler },
+    { provide: HTTP_INTERCEPTORS, useClass: ServerErrorInterceptor, multi: true }
+  ],
 })
 @Optional()
 @SkipSelf()
