f038715255e3eda20949213213c79c09fdf7e776
Flávio <florentino.flavioweb@gmail.com>
2019-12-02 15:18:35 -0300

    feat(parceiros): Changes on style

--- src/app/pages/partners/partners/partners.component.ts
+++ src/app/pages/partners/partners/partners.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faBars, faTh, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.scss']
 })
 export class PartnersComponent implements OnInit {
-
   collection = [];
-  
+  faBars = faBars;
+  faTh = faTh;
+  faChevronDown = faChevronDown;
+
   constructor() { 
     for (let i = 1; i <= 50; i++) {
       this.collection.push('item ${i}');
