37f3cae15a7a3d71ef48652b734ba39dc0d2b14e
Flávio <florentino.flavioweb@gmail.com>
2019-12-02 15:28:38 -0300

    feat(about): Forgotten files

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
-import { AboutComponent } from './about.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [AboutComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    SharedModule
   ]
 })
 export class AboutModule { }
