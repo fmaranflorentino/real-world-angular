f4051ca23a72ae567731c59cee1271c073e0a80a
Flávio <florentino.flavioweb@gmail.com>
2019-12-02 16:18:45 -0300

    feat(terms): Created terms page

---
+++ src/app/pages/landing/terms/terms.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-terms',
+  templateUrl: './terms.component.html',
+  styleUrls: ['./terms.component.scss']
+})
+export class TermsComponent implements OnInit {
+  faDownload = faDownload;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
