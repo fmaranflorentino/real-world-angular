fa82b6b77b9635f3e17c59f087552b2e4519b9fe
Flávio <florentino.flavioweb@gmail.com>
2019-12-04 17:24:20 -0300

    feat(components/modal-login): Created form validation and feedback on the view

--- src/app/shared/components/login-modal/login-modal.component.ts
+++ src/app/shared/components/login-modal/login-modal.component.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { MatDialogRef } from '@angular/material';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { fadeIn } from '../../helpers/animations';
 
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
-  styleUrls: ['./login-modal.component.scss']
+  styleUrls: ['./login-modal.component.scss'],
+  animations: [ fadeIn ]
 })
 export class LoginModalComponent implements OnInit {
   faTimes = faTimes;
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<any>,
+  ) { }
 
   ngOnInit() {
+    this.createLoginForm();
+  }
+
+  createLoginForm(): void {
+    this.loginForm = new FormGroup({
+      user: new FormControl(null, { validators: Validators.required }),
+      password: new FormControl(null, { validators: Validators.required }),
+    });
+  }
+
+  closeModal(): void {
+    this.dialogRef.close();
+  }
+
+  get controls(): any {
+    return this.loginForm.controls;
+  }
+
+  get formValidation(): boolean {
+    return this.loginForm.valid;
   }
 
 }
