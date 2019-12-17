d9d841fc6990db87269cbf804e6acfbd6ef8c8c9
Flávio <florentino.flavioweb@gmail.com>
2019-12-17 15:44:09 -0300

    feat(routing): Added listener to route events, scrolltop on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { slideInAnimation } from './shared/helpers/animations';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { wait } from './shared/helpers/ui.helper';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations: [ slideInAnimation ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'marketclub-ui';
+  token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NzYyNjYzNDMsImV4cCI6MTU3NjAwNzE0MywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.lp85wf8ggYRPN0Lq1ImDjW0oC5qrhYMKQCGU6rg84Xo'
 
-  prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        wait(100)
+        .then(() => {
+          document.body.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+      }
+  });
   }
+
 }
