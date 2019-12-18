b7fa3f635cc0dc90c4239ab7da55c7bf3a6333c6
Flávio <florentino.flavioweb@gmail.com>
2019-12-18 18:57:46 -0300

    feat(env): Added initial end-points path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
+const productBase = '/api/Produtos/{id}';
+const sellersBase = '/api/Lojas';
+const attributesBase = '/api/Atributos';
+
 export const environment = {
   production: false,
   api: {
@@ -6,11 +10,34 @@ export const environment = {
     protocol: '',
     domains: {
       auth: {
-        login: '',
+        login: '/api/Marketclub/Security/Login',
         logout: '',
       },
       products: {
         all: '',
+        search: '/api/Produtos/Pesquisa',
+        byId: productBase,
+        addProduct: '/api/Produtos',
+        editProduct: productBase,
+        deleteProduct: productBase,
+      },
+      sellers: {
+        all: sellersBase,
+        byId: `${sellersBase}/{id}`,
+        addSeller: sellersBase,
+        editSellerAvailability: `${sellersBase}/{id}`,
+        deleterSeller: `${sellersBase}/{id}`,
+      },
+      attributes: {
+        getAttributes: attributesBase,
+        editAttributes: `${attributesBase}/{chave}/{valor}`,
+        deleteAttributes: `${attributesBase}/{chave}/{valor}`,
+        types: {
+          getTypes: `${attributesBase}/Tipos`,
+          getTypesByName: `${attributesBase}/Tipos/{nome}`,
+          editTypesByName: `${attributesBase}/Tipos/{nome}`,
+          deleteTypesByName: `${attributesBase}/Tipos/{nome}`,
+        }
       },
       user: {
         financing: '',
